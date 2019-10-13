800830cd3d3792e5a3515bb917457107dec58cf9
unknown <vidhushah19@gmail.com>
2019-10-13 16:52:58 +0530

    thirdcommit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -8,3 +8,7 @@ def post_list(request):
     post_list = Post.objects.order_by('published_date')
     #filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request,'blog/post_list.html',{'posts':post_list})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
